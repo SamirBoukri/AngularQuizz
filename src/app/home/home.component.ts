46ddaca846426f4c21ca118e9cc7cc3c93446526
SamirBoukri <samir.boukri.etud@gmail.com>
2023-09-26 09:44:22 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  tab: any[] = [{question:"Question1", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question2", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question3", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question4", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question5", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question6", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question7", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question8", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question9", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"},
+                {question:"Question10", reponse1:"Reponse1", reponse2:"Reponse2", reponse3:"Reponse3", reponse4:"Reponse4"}];
+
+}
